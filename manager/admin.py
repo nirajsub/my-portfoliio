53d50396c4f48cfc1208c29f9434bb47b114d06e
nirajsub <78416265+nirajsub@users.noreply.github.com>
2022-02-08 11:42:07 +0545

    Delete manager directory

--- manager/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import*
-
-admin.site.register(
-    [Planguage, Dtool, skills, Project, ProjectLanguage, ProjectTool, Contact, Framework, DataBase, ProjectFramework, ProjectDatabase]
-)
